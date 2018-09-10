4be1d0bd3f43951b5538886c48c76db59da73fbc
giustoe82 <marcog.swe@gmail.com>
2018-09-10 11:56:31 +0200

    Initial Commit

---
+++ Travel Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel Diary
+//
+//  Created by Marco Giustozzi on 2018-09-10.
+//  Copyright © 2018 marcog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
